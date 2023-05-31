21d802e32f9fddf7923e1463b2178b664979bfff
CrazyVince <Vincentas.vic@gmail.com>
2023-05-31 19:37:49 +0300

    ok
    haha

--- Assets/Scripts/Stress.cs
+++ Assets/Scripts/Stress.cs
@@ -1,15 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Stress : MonoBehaviour
 {
     public float stress;
     public float stressGainRate;
 
+    public Image bar;
+
     private void Update()
     {
         stress += stressGainRate * Time.deltaTime;
+        bar.fillAmount = stress / 100;
+
         if (stress >= 100f)
         {
             //lose
